f8059980353d4ff2d8811fb85fc33bef5da0018a
Adrian Gette <adrian.gette@icloud.com>
2021-07-29 16:36:32 -0300

    Pasar información del formulario al action

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -4,9 +4,15 @@ import {
     AGREGAR_PRODUCTO_ERROR
 } from '../types';
 
+// en el action vamos a tener una función que se
+// tiene que utilizar en el componente, de esa
+// forma los datos de los componentes se pueden
+// pasar a las acciones y utilizamos dispatch
+// para ejecutar esas acciones
+
 // crear nuevos productos
-export function crearNuevoProductoAction() {
+export function crearNuevoProductoAction(producto) {
     return () => {
-        console.log('Desde action');
+        console.log(producto);
     }
 }
--- src/types/index.js
+++ src/types/index.js
@@ -1,3 +1,6 @@
+// en redux definimos los types que van a ir describiendo
+// lo que pasa en la aplicación
+
 export const AGREGAR_PRODUCTO = 'AGREGAR_PRODUCTO';
 export const AGREGAR_PRODUCTO_EXITO = 'AGREGAR_PRODUCTO_EXITO';
 export const AGREGAR_PRODUCTO_ERROR = 'AGREGAR_PRODUCTO_ERROR';
